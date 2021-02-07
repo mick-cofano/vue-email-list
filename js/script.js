1a6b9f4658ddcae2836797fddaeec67ca27dd660
Michele Cofano <brutalmachinelol@gmail.com>
2021-02-07 23:34:13 +0100

    Completamento esercizio. Aggiunta funzione in VueJS e stampa a schermo

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 // Eseguire in Vue.js una chiamata ajax, generare 10 email e stamparle a schermo.
 // La chiamata ajax vi ritornerà ogni volta un'email random.
+
+const app = new Vue({
+  el: "#app",
+  data: {
+    eMails: []
+  },
+
+  // ciclo che genera le 10 mail random tramite axios
+  mounted: function() {
+    for (let i = 0; i < 10; i++) {
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then(risposta => {
+        this.eMails.push(risposta.data.response) // push delle mail
+     });
+    }
+  }
+});
